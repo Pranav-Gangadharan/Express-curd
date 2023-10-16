e8eaae408f606c81fe34881a98db903cbc875e80
Pranav-Gangadharan <pranavgangadharan@yahoo.com>
2023-10-16 12:40:51 +0530

    installed cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const AppRoutes = require('./src/routes');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 app.use(express.json());
 const dotenv = require('dotenv');
 dotenv.config();
